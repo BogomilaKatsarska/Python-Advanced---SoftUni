90eebdc2fbe58d5934efef8350e5a3e48f49ca94
BogomilaKatsarska <90954520+BogomilaKatsarska@users.noreply.github.com>
2021-09-20 12:31:27 +0300

    Add files via upload

---
+++ Lists as Stacks and Queues - L1Q2.py
@@ -0,0 +1,9 @@
+expression = input()
+stack = []
+
+for index in range(len(expression)):
+    if expression[index] == "(":
+        stack.append(index)
+    elif expression[index] == ")":
+        start_index = stack.pop()
+        print(f"{expression[start_index:index+1]}")
