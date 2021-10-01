1713fcb7b4bd6ede41af8c66b131c813b85683ba
BogomilaKatsarska <90954520+BogomilaKatsarska@users.noreply.github.com>
2021-10-01 10:21:16 +0300

    Update Lists as Stacks and Queues - L1Q5.py

--- Lists as Stacks and Queues - L1Q5.py
+++ Lists as Stacks and Queues - L1Q5.py
@@ -1,3 +1,4 @@
+# OPTION I:
 from queue import deque
 
 children = deque(input().split())
@@ -9,3 +10,15 @@ while len(children) > 1:
     print(f"Removed {children.pop()}")
 
 print(f"Last is {children.popleft()}")
+
+# OPTION II:
+# from collections import deque
+# 
+# potatoes = input().split(' ')
+# potatoes_queue = deque(potatoes)
+# step = int(input())
+# 
+# while potatoes_queue:
+#     for _ in range(step-1):
+#         potatoes_queue.append(potatoes_queue.popleft())
+#     print(f'{potatoes_queue.popleft()} is out')
