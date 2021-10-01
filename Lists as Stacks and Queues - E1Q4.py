d71678a8933b211cd19d86e5a2b26f0a82fa6928
BogomilaKatsarska <90954520+BogomilaKatsarska@users.noreply.github.com>
2021-10-01 14:51:58 +0300

    Add files via upload

---
+++ Lists as Stacks and Queues - E1Q4.py
@@ -0,0 +1,18 @@
+clothes = list(map(int, input().split()))
+rack_capacity = int(input())
+
+used_racks = 1
+current_rack_capacity = rack_capacity
+
+while clothes:
+    clothing = clothes[-1]
+    if clothing > current_rack_capacity:
+        used_racks += 1
+        current_rack_capacity = rack_capacity
+    else:
+        current_rack_capacity -= clothing
+        clothes.pop()
+
+print(used_racks)
+
+
